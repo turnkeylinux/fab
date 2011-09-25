26a4122f6bb6fc59f3f96e3735dffad7f810f496
Liraz Siri <liraz@turnkeylinux.org>
2011-09-25 22:55:36 +0300

    added example opts parsing code

---
+++ tests/parseopts.py
@@ -0,0 +1,11 @@
+import sys
+import getopt
+
+opts, args = getopt.getopt(sys.argv[1:], "I:D:")
+
+for opt, val in opts:
+    if opt == '-I':
+        print "INCLUDE: " + val
+    elif opt == '-D':
+        print "DEFINE: " + val
+    
