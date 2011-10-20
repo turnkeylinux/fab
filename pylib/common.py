12016a20089be74ca860fd5bec2b0e2e004ee734
Alon Swartz <alon@turnkeylinux.org>
2011-10-20 16:52:23 +0200

    raise error if FAB_TMPDIR is not absolute path

--- pylib/common.py
+++ pylib/common.py
@@ -3,6 +3,9 @@ import os
 import sys
 import tempfile
 
+class Error(Exception):
+    pass
+
 def mkdir(path):
     path = str(path)
     if not os.path.exists(path):
@@ -11,6 +14,9 @@ def mkdir(path):
 def get_tmpdir():
     """return unique temporary directory path"""
     tmpdir = os.environ.get('FAB_TMPDIR', '/var/tmp')
+    if not os.path.isabs(tmpdir):
+        raise Error('FAB_TMPDIR is not absolute path')
+
     mkdir(tmpdir)
     return tempfile.mkdtemp(prefix="fab", dir=tmpdir)
 
