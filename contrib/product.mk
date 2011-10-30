a0ebd75bcda1ee68aea3794576b38e2d14a09961
Liraz Siri <liraz@turnkeylinux.org>
2011-10-30 23:27:06 +0200

    patched remove-deck define so that it echos the deck -D operation

--- contrib/product.mk
+++ contrib/product.mk
@@ -48,6 +48,7 @@ define remove-deck
 			echo "error: root.tmp is dirty with manual changes. To continue: deck -D $(strip $1)"; \
 			exit 1; \
 		fi; \
+		echo deck -D $1; \
 		deck -D $1; \
 	fi
 endef
