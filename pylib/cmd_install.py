0855d951276ef2048cc7874bfc64213f1d998f05
Alon Swartz <alon@turnkeylinux.org>
2011-11-03 23:47:55 +0200

    extended fab-install to support plan preprocessing

--- pylib/cmd_install.py
+++ pylib/cmd_install.py
@@ -9,6 +9,7 @@ Argument:
     
 Option:
   -i --input <file> File from which we read package list (- for stdin)
+                    Plan and spec type input are accepted
   -p --pool         Mandatory: Relative or absolute pool path
                                Defaults to environment: POOL
   --no-deps         Do not resolve and install package dependencies
@@ -21,32 +22,36 @@ import sys
 import getopt
 
 import help
+import cpp
 from plan import Plan
 from installer import Installer
 from common import get_poolpath, fatal
 
-# TODO:
-#    add cpp support
-
-@help.usage(__doc__)
+@help.usage(__doc__ + cpp.__doc__)
 def usage():
     print >> sys.stderr, "Syntax: %s [-options] <chroot> [ package[=version] ... ]" % sys.argv[0]
 
+def plan_resolve(cpp_opts, pre_processed, pool_path, resolve_deps):
+    cpp_opts += [ ("-U", "linux") ]
 
-def read_packages(fh):
-    packages = set()
-    for line in fh.readlines():
-        line = re.sub(r'#.*', '', line)
-        line = line.strip()
-        if not line:
-            continue
-        packages.add(line)
-    return packages
+    plan = Plan(pool_path)
     
+    print "processing as if plan..."
+    plan.process("\n".join(pre_processed), cpp_opts)
+    packages = set(plan.packages)
+
+    if resolve_deps:
+        print "resolving dependencies..."
+        spec = plan.resolve_to_spec()
+        packages.update(spec.list())
+
+    return packages
+
 def main():
+    cpp_opts, args = cpp.getopt(sys.argv[1:])
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], 'ip:',
-                                      ['input=', 'pool=', 'no-deps'])
+        opts, args = getopt.gnu_getopt(args, 'ip:',
+                                   ['input=', 'pool=', 'no-deps'])
     except getopt.GetoptError, e:
         usage(e)
 
@@ -71,28 +76,23 @@ def main():
             opt_resolve_deps = False
 
     chroot_path = args[0]
+    pre_processed = args[1:]
+
+    if input is not None:
+        pre_processed.extend(input.readlines())
+
+    pool_path = get_poolpath(pool_path)
+    
     if not os.path.isdir(chroot_path):
         fatal("chroot does not exist: " + chroot_path)
 
-    pool_path = get_poolpath(pool_path)
+    packages = plan_resolve(cpp_opts, pre_processed, pool_path,
+                            opt_resolve_deps)
 
-    packages = set(args[1:])
-    if input is not None:
-        packages.update(read_packages(input))
-
-    if opt_resolve_deps:
-        print "resolving dependencies..."
-        plan = Plan(pool_path)
-        plan.packages.update(packages)
-        
-        spec = plan.resolve_to_spec()
-        packages.update(spec.list())
-    
     installer = Installer(chroot_path, pool_path)
     installer.install(packages)
 
 
-        
 if __name__=="__main__":
     main()
 
