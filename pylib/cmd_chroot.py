f07dd633e35ebc1ef5aff6c9fb152888da442695
Liraz Siri <liraz@turnkeylinux.org>
2011-11-11 06:53:55 +0200

    fab-chroot: added -s --script option for running a script inside the chroot

--- pylib/cmd_chroot.py
+++ pylib/cmd_chroot.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
-"""Executes command in a new root
+"""Executes commands in a new root
+
+Options:
+  -s --script=PATH	 Run script inside chroot
+  
 """
 import os
+from os.path import *
+import paths
+
 import sys
 import getopt
+import tempfile
+import shutil
 
 import help
 from chroot import Chroot
@@ -12,23 +21,53 @@ from common import fatal
 @help.usage(__doc__)
 def usage():
     print >> sys.stderr, "Syntax: %s <newroot> [ command ... ]" % sys.argv[0]
+    print >> sys.stderr, "Syntax: %s <newroot> --script path/to/executable [ args ]" % sys.argv[0]
+
+def chroot_script(newroot, script_path, *args):
+    if not isfile(script_path):
+        fatal("not a file: " + script_path)
+
+    chroot = Chroot(newroot)
+    tmpdir = tempfile.mkdtemp(dir=join(chroot.path, "tmp"),
+                              prefix="chroot-script.")
+
+    script_path_chroot = join(tmpdir, basename(script_path))
+    shutil.copy(script_path, script_path_chroot)
+    
+    os.chmod(script_path_chroot, 0755)
+    chroot.system(paths.make_relative(chroot.path, script_path_chroot),
+                  *args)
+    shutil.rmtree(tmpdir)
 
 def main():
-    args = sys.argv[1:]
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 's:', [ 'script=' ])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    script_path = None
+    for opt, val in opts:
+        if opt in ('-s', '--script'):
+            script_path = val
+
     if not args:
         usage()
     
-    if len(args) == 1:
-        args.append("/bin/bash")
-        
     newroot = args[0]
+    args = args[1:]
     
-    if not os.path.isdir(newroot):
-        fatal("chroot does not exist: " + newroot)
+    if not isdir(newroot):
+        fatal("not a directory: " + newroot)
 
-    chroot = Chroot(newroot)
-    chroot.system(*args[1:])
+    if script_path:
+        chroot_script(newroot, script_path, *args)
         
+    else:
+        if not args:
+            args = ('/bin/bash',)
+
+        Chroot(newroot).system(*args)
+
 if __name__=="__main__":
     main()
 
