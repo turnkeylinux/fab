37e6a9604022c83d1a729cf6f11563776077fe3a
Alon Swartz <alon@turnkeylinux.org>
2011-12-06 20:29:37 +0200

    updated bootstrap.mk to be imported

--- contrib/bootstrap.mk
+++ contrib/bootstrap.mk
@@ -1,8 +1,16 @@
-RELEASE = rocky2
+#!/usr/bin/make -f
 
-FAB_PATH=/turnkey/fab
-POOL_PATH=$(FAB_PATH)/pools/$(RELEASE)
-BOOTSTRAP_LIBEXEC=/turnkey/private/fab/contrib/bootstrap
+ifndef FAB_PATH
+$(error FAB_PATH not defined - needed for default paths)
+endif
+
+ifndef RELEASE
+$(warning RELEASE not defined - default paths such as POOL may break)
+endif
+
+# default locations
+POOL ?= $(FAB_PATH)/pools/$(RELEASE)
+BOOTSTRAP_LIBEXEC ?= /turnkey/private/fab/contrib/bootstrap
 
 all: $(RELEASE)
 
@@ -11,10 +19,10 @@ clean:
 	-rm -rf base.spec required.spec $(RELEASE).repo
 
 required.spec: plan/required
-	fab-plan-resolve --output=$@ --pool=$(POOL_PATH) plan/required
+	fab-plan-resolve --output=$@ --pool=$(POOL) plan/required
 
 base.spec: plan/base required.spec
-	fab-plan-resolve --output=$@.tmp --pool=$(POOL_PATH) plan/base
+	fab-plan-resolve --output=$@.tmp --pool=$(POOL) plan/base
 	$(BOOTSTRAP_LIBEXEC)/exclude_spec.py $@.tmp required.spec > $@
 	rm -f $@.tmp
 
@@ -27,7 +35,7 @@ $(RELEASE).spec: required.spec base.spec
 
 $(RELEASE).repo: $(RELEASE).spec
 	mkdir -p $@/pool/main
-	POOL_DIR=$(POOL_PATH) pool-get -s -t -i $(RELEASE).spec $@/pool/main
+	POOL_DIR=$(POOL) pool-get -s -t -i $(RELEASE).spec $@/pool/main
 
 	$(BOOTSTRAP_LIBEXEC)/repo_index.sh $(RELEASE) main $@
 	$(BOOTSTRAP_LIBEXEC)/repo_release.sh $(RELEASE) main `pwd`/$@
