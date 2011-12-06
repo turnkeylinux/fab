61bdea185d2d6bc17caaafae2bfff622a261b13b
Alon Swartz <alon@turnkeylinux.org>
2011-12-06 20:22:19 +0200

    imported bootstrap libexec script from old bootstraps project

---
+++ contrib/bootstrap/bootstrap_spec.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import os
+import re
+import sys
+
+class Error(Exception):
+    pass
+
+def usage(s=None):
+    if s: print >> sys.stderr, s
+    print >> sys.stderr, "Syntax: %s <release> <spec> <repo_path>" % os.path.basename(sys.argv[0])
+    sys.exit(1)
+
+def system(command, *args):
+    command = command + " " + " ".join([commands.mkarg(arg) for arg in args])
+    err = os.system(command)
+    if err:
+        raise Error("command failed: " + command,
+                    os.WEXITSTATUS(err))
+
+def parse_bootstrap_spec(raw):
+    required  = []
+    base = []
+    sections = {"# REQUIRED": required,
+                "# BASE":     base}
+    for line in raw:
+        line = line.strip()
+        if line in sections:
+            section = sections[line]
+            continue
+
+        if line:
+            name, version = line.split("=")
+            section.append(name)
+
+    return required, base
+
+def main():
+    if len(sys.argv) != 4:
+        usage()
+
+    release = sys.argv[1]
+    spec = sys.argv[2]
+    repo = sys.argv[3]
+
+    if not os.path.isabs(repo):
+        usage("repository must be absoluate path: " + repo)
+
+    raw = file(spec, "r").readlines()
+    required, base = parse_bootstrap_spec(raw)
+ 
+    os.environ["REQUIRED_PACKAGES"] = " ".join(required)
+    os.environ["BASE_PACKAGES"] = " ".join(base)
+
+    system("debootstrap --arch i386 %s %s file://%s" % (release, release, repo))
+
+
+if __name__=="__main__":
+    main()
---
+++ contrib/bootstrap/repo_index.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -eq "3" ]; then
+    RELEASE=$1
+    COMPONENT=$2
+    REPO=$3
+else
+    echo "Syntax: ${0} <release> <component> <repo>"
+    exit 1
+fi
+
+mkdir -p ${REPO}/dists/${RELEASE}/${COMPONENT}/binary-i386
+
+cd ${REPO} && \
+    apt-ftparchive packages pool/${COMPONENT} > \
+    dists/${RELEASE}/${COMPONENT}/binary-i386/Packages
+
