243a24b60ed57b86dd1992e6376870ecb1eec094
Liraz Siri <liraz@turnkeylinux.org>
2011-12-08 11:01:52 +0200

    instead of checking if repo is an absolute path, resolve it into an absolute path

--- share/bootstrap/bootstrap_spec.py
+++ share/bootstrap/bootstrap_spec.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 
 import os
-import re
 import sys
 
 from executil import system
 
 def usage(s=None):
     if s: print >> sys.stderr, s
-    print >> sys.stderr, "Syntax: %s release target repo required_spec base_spec" % os.path.basename(sys.argv[0])
+    print >> sys.stderr, "Syntax: %s release target path/to/repo required_spec base_spec" % os.path.basename(sys.argv[0])
     sys.exit(1)
 
 def get_packages(spec_file):
@@ -25,14 +24,11 @@ def main():
     required_spec = sys.argv[4]
     base_spec = sys.argv[5]
 
-    if not os.path.isabs(repo):
-        usage("repository must be absolute path: " + repo)
-
     os.environ["REQUIRED_PACKAGES"] = " ".join(get_packages(required_spec))
     os.environ["BASE_PACKAGES"] = " ".join(get_packages(base_spec))
+    repo = os.path.abspath(repo)
 
     system("debootstrap --arch i386 %s %s file://%s" % (release, target, repo))
-
 
 if __name__=="__main__":
     main()
