97334d2546d009f7829dc9baa44d24611be1a960
Liraz Siri <liraz@turnkeylinux.org>
2011-12-08 11:46:56 +0200

    release -> suite (consistency with debootstrap terminology)

--- share/bootstrap/bootstrap_spec.py
+++ share/bootstrap/bootstrap_spec.py
@@ -8,7 +8,7 @@ from executil import system
 
 def usage(s=None):
     if s: print >> sys.stderr, s
-    print >> sys.stderr, "Syntax: %s release target path/to/repo required_spec base_spec" % basename(sys.argv[0])
+    print >> sys.stderr, "Syntax: %s suite target path/to/repo required_spec base_spec" % basename(sys.argv[0])
     sys.exit(1)
 
 def get_packages(spec_file):
@@ -20,13 +20,13 @@ def main():
     if len(args) != 5:
         usage()
 
-    release, target, repo, required_spec, base_spec = args
+    suite, target, repo, required_spec, base_spec = args
 
     os.environ["REQUIRED_PACKAGES"] = " ".join(get_packages(required_spec))
     os.environ["BASE_PACKAGES"] = " ".join(get_packages(base_spec))
     repo = abspath(repo)
 
-    system("debootstrap --arch i386 %s %s file://%s" % (release, target, repo))
+    system("debootstrap --arch i386 %s %s file://%s" % (suite, target, repo))
 
 if __name__=="__main__":
     main()
